4d67257407ee36a720e925ecdd43dc14d3d56f5f
Robin <robinlooup@gmail.com>
2024-12-09 13:03:17 +0100

    ecran sign up

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import HomeScreen from './screens/HomeScreen';
 import EventsScreen from './screens/EventsScreen';
 import StoriesScreen from './screens/StoriesScreen';
 import FavoritesScreen from './screens/FavoritesScreen'
+import SignUpScreen from './screens/SignUpScreen'
+import SignInScreen from './screens/SignInScreen'
+
+import user from './reducers/user'
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit'
+
+const store = configureStore({
+  reducer: {user},
+ })
 
 
 
@@ -23,6 +34,8 @@ function TabNavigator() {
         } else if (route.name === 'Histoire') {
           iconName = 'map-pin';
         } else if (route.name === 'Favoris') {
+          iconName = 'map-pin'
+        } else if (route.name === 'Home') {
           iconName = 'map-pin'
         }
 
@@ -43,12 +56,15 @@ function TabNavigator() {
 export default function App() {
   return (
     
+    <Provider store ={store}>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+          <Stack.Screen name="SignIn" component={SignInScreen} />
           <Stack.Screen name="TabNavigator" component={TabNavigator} />
         </Stack.Navigator>
       </NavigationContainer>
-    
+      </Provider>
   )
 };
