febb30334457bbbd9ad3cc2635cc3967ce5fab38
Your Elina <elina.marcol@gmail.com>
2024-12-10 13:09:43 +0100

    màj code

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
+
+import { useDispatch, useSelector } from 'react-redux';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import {
   SafeAreaView,                                                                                                                                                                                                                                                              feAreaView,
   ScrollView,
@@ -15,79 +15,85 @@ import {
   GreySeparator,
   FlatList,
   Dimensions,
-} from "react-native";
-import Popover from "react-native-popover-view";
-import Tooltip from 'rn-tooltip';
-
+} from 'react-native';
+ 
+ 
 export default function DashboardScreen() {
- //faire une const databook avec un .map pour rapeller les livres en lecture 
-
+ 
+  const data = [
+    { id: '1', image: require('../assets/avatar.png') },
+    { id: '2', image: require('../assets/avatar.png') },
+    { id: '3', image: require('../assets/avatar.png') },
+  ];
+ 
+  const Carousel = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+ 
+    const onScroll = (event) => {
+      const contentOffsetX = event.nativeEvent.contentOffset.x;
+      const index = Math.round(contentOffsetX / Dimensions.get('window').width);
+      setCurrentIndex(index);
+    };
+  };
+ 
   // const dispatch = useDispatch()
   const user = useSelector((state) => state.user.value);
-
+ 
   const [isParameterVisible, setIsParameterVisible] = useState(false);
-
-
+ 
+  const toggleParameter = () => {
+    setIsParameterVisible(!isParameterVisible);
+  };
+ 
   return (
-    <SafeAreaView style={styles.container}>
+ 
+    <SafeAreaView style={styles.container} >
       <View style={styles.header}>
+ 
         {/* Logo et Nom de l'app */}
         <View style={styles.identityApp}>
           <Image
             source={require('../assets/LogoBc.png')}
             style={styles.logo}
           />
-          <Text style={styles.title}>BookConnect</Text>
-
+ 
           {/* Icône Paramètre */}
         </View>
         <TouchableOpacity onPress={toggleParameter} style={styles.ParameterButton}>
-          <FontAwesome name="gear" size={50} color="#a2845e" />
+          <FontAwesome name="gear" size={40} color="#a2845e" />
         </TouchableOpacity>
       </View>
-
+ 
       {/* Modal */}
       <Modal
-        animationType="slide"
-        transparent={true}
-        visible={isParameterVisible}
-        onRequestClose={toggleParameter}
-      >
-        <View style={styles.ParameterOverlay}>
-          <View style={styles.ParameterContent}>
-            <Text style={styles.ParameterTitle}>Paramètres</Text>
-
-            {/* Options de paramètres */}
-            <TouchableOpacity style={styles.optionButton}>
-              <Text style={styles.optionText}>Modifier ma photo de profil</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Text style={styles.optionText}>Modifier mon username</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.optionButton}>
-              <Text style={styles.optionText}>Modifier mon email</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.optionButton}>
-              <Text style={styles.optionText}>Modifier mon mot de passe</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.optionButton}>
-              <Text style={styles.optionText}>Déconnexion</Text>
-            </TouchableOpacity>
-
-            {/* Bouton Fermer */}
-            <TouchableOpacity onPress={toggleParameter} style={styles.closeButton}>
-              <FontAwesome name="close" size={30} color="#a2845e" />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
-
+  animationType="fade"
+  transparent={true}
+  visible={isParameterVisible}
+  onRequestClose={toggleParameter}
+>
+  <View style={styles.ParameterContent}>
+ 
+    {/* Options de paramètres */}
+    <TouchableOpacity style={styles.optionButton}>
+      <Text style={styles.optionText}>Déconnexion</Text>
+    </TouchableOpacity>
+ 
+    {/* Bouton Fermer */}
+    <TouchableOpacity onPress={toggleParameter} style={styles.closeButton}>
+      <FontAwesome name="close" size={30} color="white" />
+    </TouchableOpacity>
+  </View>
+</Modal>
+ 
       {/* Photo de profil et Message de bienvenue */}
       <View style={styles.identityUser}>
-        <Image source={require("../assets/avatar.png")} style={styles.avatar} />
-        <Text style={styles.welcome}>Hello {user.username}</Text>
+        <Image
+          source={require('../assets/avatar.png')}
+          style={styles.avatar}
+        />
+        <Text style={styles.welcome}>Hello {user?.username || 'Utilisateur'}</Text>
       </View>
-
+ 
       {/* Section carrousel mes lectures en cours */}
       <View style={styles.sectionContainer}>
         <View style={styles.section}>
@@ -98,34 +104,34 @@ export default function DashboardScreen() {
           <View>
             <Text style={styles.carrousel}>Book1</Text>
             <Image
-              source={require("../assets/avatar.png")}
+              source={require('../assets/avatar.png')}
               style={styles.avatar}
             />
           </View>
           <View>
             <Text style={styles.carrousel}>Book2</Text>
             <Image
-              source={require("../assets/avatar.png")}
+              source={require('../assets/avatar.png')}
               style={styles.avatar}
             />
           </View>
           <View>
             <Text style={styles.carrousel}>Book3</Text>
             <Image
-              source={require("../assets/avatar.png")}
+              source={require('../assets/avatar.png')}
               style={styles.avatar}
             />
           </View>
           <View>
             <Text style={styles.carrousel}>Book4</Text>
             <Image
-              source={require("../assets/avatar.png")}
+              source={require('../assets/avatar.png')}
               style={styles.avatar}
             />
           </View>
         </ScrollView>
       </View>
-
+ 
       {/* Section carrousel mes évenements plannifiés*/}
       <View style={styles.sectionContainer}>
         <View style={styles.section}>
@@ -136,21 +142,23 @@ export default function DashboardScreen() {
       </View>
     </SafeAreaView>
   );
-}
-
+};
+ 
+ 
 // attention : le StyleSheet doit bien être en dehors de la fonction!
 const styles = StyleSheet.create({
+ 
   container: {
     flex: 1,
-    backgroundColor: "white",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-
+ 
   header: {
-    position: "absolute", // Colle le header en haut
+    position: 'absolute', // Colle le header en haut
     top: 40, // Définit le haut
-    width: "100%", // Prend toute la largeur
+    width: '100%', // Prend toute la largeur
     flexDirection: "row", // En ligne
     justifyContent: "space-between",
     alignItems: "center",
@@ -158,63 +166,63 @@ const styles = StyleSheet.create({
     paddingVertical: 10, // Espacement vertical
     zIndex: 1, // Place au-dessus du contenu
   },
-
+ 
   identityApp: {
     flexDirection: "row",
-    alignItems: "center",
+    alignItems: 'center',
   },
-
+ 
   logo: {
     height: 90,
     width: 90,
   },
-
+ 
   title: {
     fontSize: 18,
-    fontWeight: "600",
-    color: "black",
+    fontWeight: '600',
+    color: 'black',
   },
-
+ 
   ParameterButton: {
     padding: 10, // Zone cliquable plus grande
   },
-
+ 
   identityUser: {
     marginTop: 10, // Décale le contenu pour éviter le header
     alignItems: "center",
     marginVertical: 20,
   },
-
+ 
   avatar: {
     width: 150,
     height: 150,
     borderRadius: 50,
     marginBottom: 10,
-    backgroundColor: "#f2f2f2",
+    backgroundColor: '#f2f2f2',
   },
-
+ 
   welcome: {
     fontSize: 24,
-    color: "#333",
+    color: '#333',
   },
-
+ 
   sectionContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center",
+    alignItems: 'center',
     paddingHorizontal: 20,
     marginBottom: 20,
   },
-
+ 
   section: {
     flex: 1,
     flexDirection: "row",
     alignItems: "center",
   },
-
+ 
   textSection: {
     fontSize: 16,
-    color: "#555",
+    color: '#555',
     marginRight: 10,
   },
   ParameterContent: {
@@ -249,17 +257,5 @@ const styles = StyleSheet.create({
     alignSelf: 'flex-end',
     marginTop: 10,
   },
-
-  popover : {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: 30,
-    width: 100,
-    backgroundColor: "transparent",
-    color:"white",
-    borderRadius: 4,
-    transitionDuration: 0.5,
-    cursor: "pointer",
-  }
+ 
 });
