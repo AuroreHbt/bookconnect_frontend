59234bf8aa995f5d49d83c082ed183104321012d
AuroreHbt <aurore.himbaut@gmail.com>
2024-12-11 16:51:00 +0100

    modif CSS margnVertical au lieu de margin sur les button

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -245,7 +245,7 @@ const styles = StyleSheet.create({
 
   button: {
     backgroundColor: "#D84815",
-    margin: 40,
+    marginVertical: 40,
     borderRadius: 10,
     padding: 10,
     justifyContent: 'center',
