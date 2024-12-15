cede54740eca66f53b049d06a75e7cfbd84d2482
Your Elina <elina.marcol@gmail.com>
2024-12-15 20:49:11 +0100

    test3

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -45,7 +45,7 @@ export default function MapScreen({ route, navigation }) {
         });
       }
     }
-  }, [eventsData]);
+  }, [eventsData]); 
 
   // Bouton "Retour"
   const goBack = () => navigation.goBack();
